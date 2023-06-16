07a78536f8ef838877dad1eae2a1241ce6cbaeb2
aksunia18091995 <98849593+aksunia18091995@users.noreply.github.com>
2023-06-16 21:29:32 +0300

    all tasks

--- homework02/script.js
+++ homework02/script.js
@@ -1,11 +1,31 @@
-const num1 = Number(prompt('Введіть перше число'));
-const num2 = Number(prompt('Введіть друге число'));
+let num1;
 
-if (Number.isInteger(num1) && Number.isInteger(num2) && num1<num2) {
-    for (num1; num1 <= num2; num1++){
-        document.writeln()
-    };
-} else {
-    
+do { 
+    num1 = Number(prompt('Введіть ціле перше число'));
+} while (
+    num1 === NaN || !Number.isInteger(num1) || !num1
+)
+
+console.log(num1);
+
+let num2;
+
+do { 
+    num2 = Number(prompt('Введіть ціле друге число'))
+} while (
+    num2 === NaN || !Number.isInteger(num2)|| !num2
+)
+
+console.log(num2);
+
+const requestEven = confirm('Пропускаємо парні числа?');
+let sum = 0;
+
+for (num1; num1 <= num2; num1++){
+    if (requestEven === true && num1 % 2 == 0) {
+        continue;
+    } 
+    sum = sum+num1;
 }
 
+document.writeln('Cума: ', sum)
