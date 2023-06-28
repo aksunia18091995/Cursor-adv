b31adc1b8edaf08bf4af6b53befb057bfafc946a
aksunia18091995 <98849593+aksunia18091995@users.noreply.github.com>
2023-06-28 23:01:49 +0300

    fixed getModa and add repeating

--- homework05/script.js
+++ homework05/script.js
@@ -1,3 +1,20 @@
+// Функція для перевірки помилок при введенні даних
+function getNumbersFromUser() {
+  let numbersString = prompt('Введіть цілі числа через пробіл');
+  while (
+    numbersString.trim() === '' ||
+    numbersString.split(' ').some(num => isNaN(Number(num))) ||
+    numbersString.split(' ').some(num => !Number.isInteger(Number(num)))
+  ) {
+    numbersString = prompt('Помилка. Введіть цілі числа через пробіл');
+  }
+
+  let numbersArray = numbersString.split(' ');
+  let numbers = numbersArray.map(numString => parseFloat(numString.trim()));
+
+  return numbers;
+}
+
 // Рандомні числа
 alert('Визначаємо рандомні числа у проміжках')
 function getRandomArray() {
@@ -37,23 +54,16 @@ const resRandomArray = getRandomArray();
 // Рахуємо середнє арифметичне
 alert('Визначаємо середнє арифметичне у ряді чисел')
 function getAverage() {
-    let numbersString = prompt('Введіть цілі числа через пробіл:');
-    while (numbersString.trim() === '' || numbersString.split(' ').some(num => isNaN(Number(num))) || numbersString.split(' ').some(num => !Number.isInteger(Number(num)))) {
-        numbersString = prompt('Помилка. Введіть цілі числа через пробіл')
-    }
-
-    let numbersArray = numbersString.split(' ');
-    let numbers = numbersArray.map(numString => parseFloat(numString.trim()));
-
-    numbers = numbers.filter(num => !isNaN(num) && Number.isInteger(num));
+    const numbers = getNumbersFromUser();
+    const validNumbers = numbers.filter(num => !isNaN(num) && Number.isInteger(num));
     if (numbers.length === 0) {
         return 'No valid numbers entered';
     }
 
-    const sum = numbers.reduce((total, num) => total + num, 0);
-    const average = sum / numbers.length;
+    const sum = validNumbers.reduce((total, num) => total + num, 0);
+    const average = sum / validNumbers.length;
     return {
-        numbers: numbers,
+        numbers: validNumbers,
         average: average
     }
     
@@ -64,22 +74,15 @@ const resAverage = getAverage();
 // Фільтруємо парні числа
 alert('Відкидаємо усі парні числа, залишаємо лише непарні')
 function filterEvenNumbers() {
-    let numbersString = prompt('Введіть цілі числа через пробіл:');
-    while (numbersString.trim() === '' || numbersString.split(' ').some(num => isNaN(Number(num))) || numbersString.split(' ').some(num => !Number.isInteger(Number(num)))) {
-        numbersString = prompt('Помилка. Введіть цілі числа через пробіл')
-    }
-
-    let numbersArray = numbersString.split(' ');
-    let numbers = numbersArray.map(numString => parseFloat(numString.trim()));
-
-    numbers = numbers.filter(num => !isNaN(num) && Number.isInteger(num)&& num % 2 !== 0);
-    if (numbers.length === 0) {
+    const numbers = getNumbersFromUser();
+    const validNumbers = numbers.filter(num => !isNaN(num) && Number.isInteger(num)&& num % 2 !== 0);
+    if (validNumbers.length === 0) {
         return 'No valid numbers entered';
     }
 
     return {
-        numbers: numbers,
-        numbersArray:numbersArray
+        numbers: validNumbers,
+        numbersArray:numbers
     }
 }
 const resOddNumber = filterEvenNumbers();
@@ -88,23 +91,16 @@ const resOddNumber = filterEvenNumbers();
 // Рахуємо лише додатні числа
 alert('Рахуємо лише кількість додатніх чисел');
 function countPositiveNumbers() {
-    let numbersString = prompt('Введіть цілі числа через пробіл:');
-    while (numbersString.trim() === '' || numbersString.split(' ').some(num => isNaN(Number(num))) || numbersString.split(' ').some(num => !Number.isInteger(Number(num)))) {
-        numbersString = prompt('Помилка. Введіть цілі числа через пробіл')
-    }
-
-    let numbersArray = numbersString.split(' ');
-    let numbers = numbersArray.map(numString => parseFloat(numString.trim()));
-
-    numbers = numbers.filter(num => !isNaN(num) && Number.isInteger(num) && num > 0);
+    const numbers = getNumbersFromUser();
+    const validNumbers = numbers.filter(num => !isNaN(num) && Number.isInteger(num) && num > 0);
     if (numbers.length === 0) {
         return 'No valid positive numbers entered';
     }
 
-    const countPositiveNumbers = numbers.length;
+    const countPositiveNumbers = validNumbers.length;
 
     return {
-        numbersArray: numbersArray,
+        numbersArray: numbers,
         countPositiveNumbers:countPositiveNumbers
     }
 } 
@@ -114,64 +110,52 @@ const resCountPosNum = countPositiveNumbers();
 
 alert('Залишамо лише числа, кратні 5');
 function getDividedByFive() {
-    let numbersString = prompt('Введіть цілі числа через пробіл:');
-    while (numbersString.trim() === '' || numbersString.split(' ').some(num => isNaN(Number(num))) || numbersString.split(' ').some(num => !Number.isInteger(Number(num)))) {
-        numbersString = prompt('Помилка. Введіть цілі числа через пробіл')
-    }
-
-    let numbersArray = numbersString.split(' ');
-    let numbers = numbersArray.map(numString => parseFloat(numString.trim()));
-
-    numbers = numbers.filter(num => !isNaN(num) && Number.isInteger(num) && num % 5 === 0);
-    if (numbers.length === 0) {
+    const numbers = getNumbersFromUser();
+    const validNumbers = numbers.filter(num => !isNaN(num) && Number.isInteger(num) && num % 5 === 0);
+    if (validNumbers.length === 0) {
         return 'No valid positive numbers entered';
     }
 
     return {
-        numbersArray: numbersArray,
-        numbers:numbers
+        numbersArray: numbers,
+        numbers:validNumbers
     }
 }
 
 const resDivBy5 = getDividedByFive();
-// document.writeln(`Серед чисел ${resDivBy5.numbersArray} є такі, що діляться націло на 5 - ${resDivBy5.numbers}`)
+document.writeln(`Серед чисел ${resDivBy5.numbersArray} є такі, що діляться націло на 5 - ${resDivBy5.numbers}`)
 
 // Визначаємо наймоднішу цифру
 alert('Визначаємо наймоднішу цифру серед ряду чисел')
 function getModa() {
-    let numbersString = prompt('Введіть цілі числа через пробіл:');
-    while (numbersString.trim() === '' || numbersString.split(' ').some(num => isNaN(Number(num))) || numbersString.split(' ').some(num => !Number.isInteger(Number(num)))) {
-        numbersString = prompt('Помилка. Введіть цілі числа через пробіл')
-    }
-
-    let numbersArray = numbersString.split(' ');
-    let numbers = numbersArray.map(numString => parseFloat(numString.trim()));
-
-    numbers = numbers.filter(num => !isNaN(num) && Number.isInteger(num));
-    if (numbers.length === 0) {
+    const numbers = getNumbersFromUser();
+    const validNumbers = numbers.filter(num => !isNaN(num) && Number.isInteger(num));
+    if (validNumbers.length === 0) {
         return 'No valid numbers entered';
     }
 
     const numbersObject = {};
-    for (let number of numbers) {
+    for (let number of validNumbers) {
         const digits = String(number).split('');
         for (let digit of digits) {
             numbersObject[digit] = numbersObject[digit] ? numbersObject[digit] + 1 : 1;
         }
     }
 
-    let moda=null;
+    let moda=[];
     let maxCount = 0;
 
     for (let digit in numbersObject) {
         if (numbersObject[digit] > maxCount) {
             maxCount = numbersObject[digit];
-            moda = digit;
+            moda = [digit];
+        } else if (numbersObject[digit] === maxCount) {
+            moda.push(digit);
         }
     }
 
     return {
-        numbers: numbers,
+        numbers: validNumbers,
         moda: moda
     }
 }
