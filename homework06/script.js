a5c52d6a21fc297ccc326627f6061ccc6fbdc79c
aksunia18091995 <98849593+aksunia18091995@users.noreply.github.com>
2023-06-29 23:30:54 +0300

    all tasks

--- homework06/script.js
+++ homework06/script.js
@@ -24,5 +24,109 @@ cosmology: [5, 5, 5, 5]
 }
 }];
 
-getSubjects(students[0] --> ["Math", "Algorithms", "Data
-science"]
+// Розподіляємо студент і предмети
+function getSubjects(student) {
+    const subjects = Object.keys(student.subjects);
+    const formattedSubjects = subjects.map(subject => {
+        const words = subject.split('_');
+        const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+        if (capitalizedWords.includes('Science')) {
+            const index = capitalizedWords.indexOf('Science');
+            capitalizedWords[index] = 'science';
+        }
+        return capitalizedWords.join(' ');
+    });
+        
+    return {
+        name: student.name,
+        subjects: formattedSubjects.map(subject=>`"${subject}"`).join(', ')
+    };
+}
+for (let i = 0; i < students.length; i++){
+    const studentSubjects = getSubjects(students[i]);
+    document.writeln(`${studentSubjects.name} вивчає [${studentSubjects.subjects}]<br>`);
+}
+    
+// Середня оцінка студента
+function getAverageMark(student) {
+    const marks = Object.values(student.subjects).flat();
+    const sum = marks.reduce((total, mark) => total + mark, 0);
+    const average = sum / marks.length;
+    return {
+        average: average.toFixed(2),
+        name:student.name
+    }
+}
+
+for (let i = 0; i < students.length; i++){
+    const studentMarkAverage = getAverageMark(students[i]);
+    document.writeln(`У ${studentMarkAverage.name} середній бал ${studentMarkAverage.average}<br>`);
+}
+
+// Інформація про студента
+function getStudentInfo(student) {
+    const infoStudent = {
+        course: student.course,
+        name: student.name,
+        averageMark: getAverageMark(student).average
+    };
+    
+    return infoStudent
+}
+for (let i = 0; i < students.length; i++){
+const infoOfStudents = getStudentInfo(students[i]);
+    document.writeln(`"Курс": ${infoOfStudents.course}, "Ім'я": "${infoOfStudents.name}", "Середня оцінка": ${infoOfStudents.averageMark}<br>`)
+}
+
+// Імена студентів
+function getStudentsNames(students) {
+    const names = students.map(student => student.name);
+    const sortedNames = names.sort();
+    return sortedNames.map(name=>`"${name}"`);
+}
+const studentNames = getStudentsNames(students);
+document.writeln(`Імена в алфавітному порядку [${studentNames}]<br>`);
+
+// Найкращий студент
+function getBestStudent(students) {
+    let bestStudent = null;
+    let bestAverageMark = 0;
+
+    for (let i = 0; i < students.length; i++){
+        const averageMark = getAverageMark(students[i]).average;
+            
+        if (averageMark > bestAverageMark) {
+            bestAverageMark = averageMark;
+            bestStudent = students[i].name;
+        }
+    }
+    return bestStudent;
+}
+const bestStudent = getBestStudent(students);
+document.writeln(`Найкращий студент - "${bestStudent}"<br>`)
+
+// Рахуємо кількість букв у слові
+function calculateWordLetters() {
+    let word = prompt('Введіть слово для перевірки')
+    while (!word || word.trim() === '' || !isNaN(word)) {
+        word = prompt('Помилка! Введене слово - некоректне'); 
+    }
+
+    const letters = {};
+
+    for (let i = 0; i < word.length; i++){
+        const letter = word[i];
+
+        if(letter!==' '){
+            if (letters.hasOwnProperty(letter)) {
+                letters[letter]++;
+            } else {
+                letters[letter] = 1;
+            }
+        }
+    }
+    return letters;
+}
+
+const wordLetters = calculateWordLetters();
+document.writeln(`Розбір вашого слова ${JSON.stringify(wordLetters)}`)
